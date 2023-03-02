af6bfbf5b61c9b3a9dc33617961d1e7b6ae8af00
242425 <242425@edu.p.lodz.pl>
2023-03-02 09:50:55 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Wybierz rodzaj funkcji: \n"
+      "1. funkcja trygometryczna\n"
+      "2. fukcja wielomianowa\n"
+      "3. funkcja wykładnicza\n")
+choice = input("Twój wybór: ")
+
+if choice == "1":
+    print("Wybór f. tryg")
+elif choice == "2":
+    print("Wybór f. wielo")
+elif choice == "3":
+    print("Wybór f. wykł")
