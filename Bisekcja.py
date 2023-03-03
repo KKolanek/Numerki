4759d5c0e0b2ffdce2d03e826ed2df1d9d84c161
242425 <242425@edu.p.lodz.pl>
2023-03-03 15:26:27 +0100

    menu 1.0.2 Bisekcja.py 1.0

--- Bisekcja.py
+++ Bisekcja.py
@@ -1,2 +1,16 @@
+def bisekcja(fun, mini, maks, itera, dokl):
+    x = 0
+    if fun(mini) * fun(maks) > 0.0:
+        return "Nie znaleziono miejsca zerowego w danym przedziale"
 
-
+    else:
+        for i in range(1, itera):
+            x = (maks + mini)/2
+            if dokl > abs(maks-mini):
+                return x
+            else:
+                if fun(x) * fun(mini) < 0:
+                    maks = x
+                else:
+                    mini = x
+    return x
--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 # Wybór funkcji
+from math import sin, cos, tan
 
-print("Wybierz rodzaj funkcji: \n"
-      "1. funkcja trygometryczna\n"
-      "2. fukcja wielomianowa\n"
-      "3. funkcja wykładnicza\n")
-choice = input("Twój wybór: ")
+from Bisekcja import bisekcja
 
-if choice == "1":
-    print("Wybór f. trygometrycznych:\n"
-          "1. sin(x)\n"
-          "2. cos(x)\n"
-          "3. sin(2x)\n"
-          "4. cos(2x)")
-    choice2 = input("Twój wybór: ")
 
-elif choice == "2":
-    stopien = input("Podaj stopień wielomianu: ")
-    for i in range(stopien, 0):
-        wspol = input("Podaj wspołczynnik przy argumencie o potędze " + stopien + ":")
+def wybor_fun():
+    wybrana = 0
+    print("Wybierz rodzaj funkcji: \n"
+          "1. funkcja trygometryczna\n"
+          "2. fukcja wielomianowa\n"
+          "3. funkcja wykładnicza\n")
+    choice = input("Twój wybór: ")
 
-elif choice == "3":
-    podst = input("Podaj podstawe funkcji wykładniczej: ")
+    if choice == "1":
+        print("Wybór f. trygometrycznych:\n"
+              "1. sin(x)\n"
+              "2. cos(x)\n"
+              "3. 2sin(x)\n"
+              "4. 2cos(x)")
+        choice2 = input("Twój wybór: ")
+        if choice2 == "1":
+            wybrana = sin
+        elif choice2 == "2":
+            wybrana = cos
+        elif choice2 == "3":
+            wybrana = tan
+        elif choice2 == "4":
+            wybrana = 1 / tan
 
-else:
-    print("Zły wybór!!!")
+    elif choice == "2":
+        stopien = input("Podaj stopień wielomianu: ")
+        for i in range(stopien, 0):
+            wspol = input("Podaj wspołczynnik przy argumencie o potędze " + stopien + ":")
+
+    elif choice == "3":
+        podst = input("Podaj podstawe funkcji wykładniczej: ")
+
+    else:
+        print("Zły wybór!!!")
+
+    return wybrana
+
 
 # Wybór metody
 
@@ -31,10 +48,12 @@ print("Wybierz metodę: \n"
       "1. Metoda bisekcji\n"
       "2. Reguła falsi\n")
 choice3 = input("Twój wybór: ")
-
+if choice3 != "1" and choice3 != "2":
+    print("Zły wybór")
 
 # Kryterium stopu
-
+itera = 1 * 10 ** 20
+dokl = 1 * 10 ** -20
 print("Wybierz kryterium stopu: \n"
       "1. Ilość iteracji\n"
       "2. Zadana dokładność\n")
@@ -52,3 +71,6 @@ print("Podaj przedział na którym chcesz znależć miejsce zerowe")
 mini = input("Wartość minimalna: ")
 maks = input("Watrość maksymalna: ")
 
+# Wynik
+if choice3 == "1":
+    print("\nWynik: " + str(bisekcja(wybor_fun(), float(mini), float(maks), int(itera), float(dokl))))
