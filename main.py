f7740618ecb4c19ee4006e44ca740e1a39a31652
242425 <242425@edu.p.lodz.pl>
2023-03-03 11:10:39 +0100

    menu 1.0.1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Wybór funkcji
+
 print("Wybierz rodzaj funkcji: \n"
       "1. funkcja trygometryczna\n"
       "2. fukcja wielomianowa\n"
@@ -14,9 +16,39 @@ if choice == "1":
 
 elif choice == "2":
     stopien = input("Podaj stopień wielomianu: ")
+    for i in range(stopien, 0):
+        wspol = input("Podaj wspołczynnik przy argumencie o potędze " + stopien + ":")
 
 elif choice == "3":
     podst = input("Podaj podstawe funkcji wykładniczej: ")
 
 else:
     print("Zły wybór!!!")
+
+# Wybór metody
+
+print("Wybierz metodę: \n"
+      "1. Metoda bisekcji\n"
+      "2. Reguła falsi\n")
+choice3 = input("Twój wybór: ")
+
+
+# Kryterium stopu
+
+print("Wybierz kryterium stopu: \n"
+      "1. Ilość iteracji\n"
+      "2. Zadana dokładność\n")
+choice4 = input("Twój wybór: ")
+if choice4 == "1":
+    itera = input("Podaj ilość iteracji: ")
+elif choice4 == "2":
+    dokl = input("Podaj dokładność: ")
+else:
+    print("Zły wybór")
+
+# Przedział
+
+print("Podaj przedział na którym chcesz znależć miejsce zerowe")
+mini = input("Wartość minimalna: ")
+maks = input("Watrość maksymalna: ")
+
