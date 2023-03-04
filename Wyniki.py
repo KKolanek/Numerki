12a54e2e053c5d2abde181b52da2d587278a90ab
kamill10 <kam.cieslak@o2.pl>
2023-03-04 13:47:29 +0100

    zlozona

--- Wyniki.py
+++ Wyniki.py
@@ -1,19 +1,28 @@
 from cmath import sin, cos
 
 
-def wykladnicza(podstawa,wykladnik):
-    return podstawa**wykladnik
+def wykladnicza(podstawa, wykladnik):
+    return podstawa ** wykladnik
+
+
 def oblicz_sin(x):
     return sin(x)
+
+
 def oblicz_cos(x):
     return cos(x)
-def oblicz_sin_2x(x):
-    return 2*sin(x)*cos(x)
-def oblicz_cos_2x(x):
-    return cos(x)**2-sin(x)**2
-def horner(wspolczynniki,stopien,wartosc_x):
-    wynik = wspolczynniki[0];
-    for i in range(1,stopien+1):
-        wynik = wynik * wartosc_x + wspolczynniki[i]
 
-    return wynik;
+
+def oblicz_sin_2x(x):
+    return 2 * sin(x) * cos(x)
+
+
+def oblicz_cos_2x(x):
+    return cos(x) ** 2 - sin(x) ** 2
+
+
+def horner(wspolczynniki, stopien, wartosc_x):
+    wynik = wspolczynniki[0]
+    for i in range(1, stopien + 1):
+        wynik = wynik * wartosc_x + wspolczynniki[i]
+    return wynik
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from math import sin, cos
 
 from Bisekcja import bisekcja
+from Wyniki import oblicz_cos_2x, oblicz_sin_2x, oblicz_sin, oblicz_cos, horner
 
 
 def wybor_fun():
@@ -9,9 +10,10 @@ def wybor_fun():
     print("Wybierz rodzaj funkcji: \n"
           "1. funkcja trygometryczna\n"
           "2. fukcja wielomianowa\n"
-          "3. funkcja wykładnicza\n")
+          "3. funkcja wykładnicza\n"
+          "4. funkcja zlozona\n")
     choice = input("Twój wybór: ")
-    
+
     if choice == "1":
         print("Wybór f. trygometrycznych:\n"
               "1. sin(x)\n"
@@ -23,16 +25,40 @@ def wybor_fun():
             wybrana = sin
         elif choice2 == "2":
             wybrana = cos
+        elif choice2 == "3":
+            wybrana = oblicz_sin_2x
+        elif choice2 == "4":
+            wybrana = oblicz_cos_2x
 
     elif choice == "2":
         stopien = input("Podaj stopień wielomianu: ")
-        wspolczynniki = [0 for i in range(int(stopien)+1)]
-        for i in range(0, int(stopien)+1):
+        wspolczynniki = [0 for i in range(int(stopien) + 1)]
+        for i in range(0, int(stopien) + 1):
             wspolczynniki[i] = int(input("Podaj wspołczynnik przy argumencie o potędze " + str(stopien) + ":"))
-
+            wybrana="wielomian",wspolczynniki,stopien
     elif choice == "3":
         podst = input("Podaj podstawe funkcji wykładniczej: ")
-
+        wybrana="wykladnicza",podst
+    elif choice == "4":
+        print("Wybierz funkcje zewneczna\n"
+              "1. sin(x)\n"
+              "2. cos(x)\n"
+              "3. sin(2x)\n"
+              "4. cos(2x)"
+              )
+        zewneczna=input("Podaj wybor")
+        stopien = input("Wpisz stopien wielomianowej funkcje wewneczna ")
+        wspolczynniki = [0 for i in range(int(stopien) + 1)]
+        for i in range(0, int(stopien) + 1):
+            wspolczynniki[i] = int(input("Podaj wspołczynnik przy argumencie o potędze " + str(stopien) + ":"))
+    if zewneczna == "1":
+        wybrana = oblicz_sin(horner(wspolczynniki,stopien))
+    elif zewneczna == "2":
+        wybrana = oblicz_cos(horner(wspolczynniki,stopien))
+    elif zewneczna == "3":
+        wybrana = oblicz_sin_2x(horner(wspolczynniki,stopien))
+    elif zewneczna== "4":
+        wybrana = oblicz_cos_2x(horner(wspolczynniki,stopien))
     else:
         print("Zły wybór!!!")
 
@@ -72,4 +98,4 @@ maks = input("Watrość maksymalna: ")
 if choice3 == "1":
     print("\nWynik: " + str(bisekcja(wybor_fun(), float(mini), float(maks), int(itera), float(dokl))))
 elif choice3 == "2":
-    print("\nWynik: ")
+    print("\nWynik: " + str(bisekcja(wybor_fun(), float(mini), float(maks),choice4, int(itera), float(dokl))))
